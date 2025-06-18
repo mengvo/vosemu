caa5ff031afa16ae54f01b089ded08d93333b448
mengvo <mengzhenchang@gmail.com>
2025-06-17 23:01:41 -0400

    finshed mapping instructions

--- src/cpu.c
+++ src/cpu.c
@@ -15,6 +15,22 @@ void cpu_init(CPU* cpu) {
 }
 
 bool cpu_step(CPU* cpu, Mem* mem) {
-    u8 instruction = read_byte(mem, cpu->PC++);
+    u8 opcode = read_byte(mem, cpu->PC++);
+    Instruction ins = instructions[opcode];
     return false;
 }
+
+/* 
+
+TODO : Finish create an execute instruction function
+
+* may need to create a new struct containing addressing mode info, like what registers/immediates are we using, and other info of instruction
+* use a switch to find out what the curr ins addressing mode is and store the needed info in that struct
+* then write functions for each ins_type and use the struct to execute, set flags, etc.
+
+void execute_ins(CPU*, Mem*, Instruction)
+1.) figure out what instruction type e.g. load, alu, conditional, etc?
+2.) fetch amt of bytes needed
+3.) call its respective function?
+
+*/
